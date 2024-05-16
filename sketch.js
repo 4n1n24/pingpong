ee51b3858804d0297bdfb5ece00b9d7f57e1b29d
4n1n24 <00001102212349sp@al.educacao.sp.gov.br>
2024-05-17 01:12:31 +1030

    add arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,83 @@
+//variáveis da bolinha
+
+let xBolinha = 100;
+let yBolinha = 200;
+let diametro = 15;
+
+//velocidade da bolinha
+
+let velocidadexBolinha=6;
+let velocidadeyBolinha =6;
+let raio= diametro/2;
+
+//variáveis da raquete
+let xRaquete = 5;
+let yRaquete = 150;
+let raqueteComprimento = 10;
+let raqueteAltura = 90;
+
+//variaveis do oponente
+let xRaqueteOponente=585;
+let yRaqueteOponente= 150;
+
+let colidiu=false
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(0);
+  mostraBolinha();
+  movimentaBolinha();
+  verificaColisaoBorda();
+  mostraRaquete();
+  movimentaMinhaRaquete();
+  //verificaColisaoRaquete();
+  colisaoMinhaRaqueteBiblioteca();
+  mostraRaquete (xRaquete, yRaquete);
+  mostraRaquete(xRaqueteOponente, yRaqueteOponete);
+}
+  function mostraBolinha() {
+  circle(xBolinha, yBolinha, diametro);
+}
+
+function movimentaBolinha() {
+  xBolinha += velocidadexBolinha;
+  yBolinha += velocidadeyBolinha;
+}
+
+function verificaColisaoBorda() {
+  if (xBolinha + raio > width || xBolinha - raio < 0) {
+    velocidadeXBolinha *= -1;
+  }
+  if (yBolinha + raio > height || yBolinha - raio < 0) {
+    velocidadeYBolinha *= -1;
+  }
+}
+
+function mostraRaquete(x,y) {
+  rect(x, y, raqueteComprimento, raqueteAltura);
+}
+
+function movimentaMinhaRaquete() {
+  if(keyIsDown(UP_ARROW)) {
+    yRaquete -= 10;
+  }
+  if(keyIsDown(DOWN_ARROW)) {
+    yRaquete += 10;
+  }
+}
+
+function verificaColisaoRaquete() {
+  if (xBolinha - raio < xRaquete + raqueteComprimento && yBolinha - raio < yRaquete + raqueteAltura && yBolinha + raio > yRaquete) {
+    velocidadeXBolinha *= -1;
+  }
+}
+
+function colisaoMinhaRaqueteBiblioteca() {
+colidiu = collideRectCircle(xRaquete, yRaquete, raqueteComprimento, raqueteAltura, xBolinha, yBolinha, raio);
+  if(colidiu) {
+    velocidadexBolinha *= -1;
+  }
+}
